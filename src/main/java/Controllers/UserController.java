c978363799cc8554298301e9f5b862383464563b
A <artin.johary81@gmail.com>
2024-12-23 23:19:43 +0330

    Update

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -1,4 +1,26 @@
 package Controllers;
 
+import Entities.User;
+import Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/auth/register")
+    public void register(@RequestBody User user) {
+        userService.save(user);
+    }
+
+    @RequestMapping("/users/{username}")
+    public List<User> getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+
 }
